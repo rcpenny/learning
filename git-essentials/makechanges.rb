c9046a1b9ccff48310ac32242abfe1d40a30ab03
rcpenny <rongchen0905@gmail.com>
2020-03-23 18:10:32 +0800

    make changes

--- git-essentials/makechanges.rb
+++ git-essentials/makechanges.rb
@@ -1,3 +1,15 @@
 # add files
 git add <filename1> <filename2> ...
 git add .
+
+# view changes
+git diff (compare working tree with stage)
+
+# view only staged changes
+git diff --cached
+
+# delete files
+git rm <filename>
+
+# move rename files
+git mv <prev_filename> <new_filename>
