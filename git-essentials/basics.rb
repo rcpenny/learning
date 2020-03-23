6a289d50d4511d71d26cf6c7d462dd3c80be3896
rcpenny <rongchen0905@gmail.com>
2020-03-23 17:11:17 +0800

    basics

---
+++ git-essentials/basics.rb
@@ -0,0 +1,18 @@
+## git intro
+## https://rogerdudler.github.io/git-guide/index.zh.html
+
+## git tips
+## https://github.com/521xueweihan/git-tips
+
+
+git config
+git init
+
+git add . # . means add all changes in the current dir
+git commit -m "first commit" # commit
+
+# 1. make changes 2. add changes 3. commit changes to the repo with message
+
+
+# view the commit log
+git log
--- git-essentials/basics.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git config --global
-git config --list
