55fa834fb2f6d1e60966641fa72eb2ef29bb8a15
rcpenny <rongchen0905@gmail.com>
2020-03-23 17:24:17 +0800

    concepts

---
+++ git-essentials/concepts.rb
@@ -0,0 +1,11 @@
+https://www.linkedin.com/learning/git-essential-training-the-basics/
+
+# three trees
+working copy  -> staging index -> repo
+            git add         git commit
+
+# git workflow
+add change sets
+
+# hash values(SHA-1)
+git generates a checksum(hash value) for each change set
