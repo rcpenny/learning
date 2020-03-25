065bda7fffcb1fc8c6cc365804e349e8eeeb6d88
rcpenny <rongchen0905@gmail.com>
2020-03-25 16:53:04 +0800

    use case user stories

---
+++ oop-foundations/UseCaseStory.cs
@@ -0,0 +1,18 @@
+// Use Cases
+	1. Title: what is the goal?
+	2. Primary Actor: who desires it?
+	Application initiation distinguishes the primary actor
+	3. Success scenario: how is it accomplished? 
+		a. Scenario as paragraph
+		b. Scenario as steps
+
+Additional details:
+	• Preconditions, postconditions
+	• Secondary actor
+	• Etc.
+
+// Use stories
+informal sentences:
+	1. As a (type of user)
+	2. I want (goal)
+So that (reason)
