ae8ec04bd62487bbf76c11901c094bcde0423a22
rcpenny <rongchen0905@gmail.com>
2020-03-25 16:49:12 +0800

    requirements

---
+++ oop-foundations/Requirements.cs
@@ -0,0 +1,19 @@
+// Functional Requirements
+What must it do?
+The system/application must do...
+
+// Non-Functional Requirements
+How should it do it?
+The system/application should be...
+
+// FURPS Requirements
+	• Functionality: 
+	Capability, reusability, security
+	• Usability: 
+	Human factors, aesthetics, consistency, documentation
+	• Reliability: 
+	Availability, Failure rate, duration, predictability
+	• Performance: 
+	Speed, efficiency, resource consumption, scalability
+	• Supportability: 
+Testability, Extensibility, Serviceability, configurability
