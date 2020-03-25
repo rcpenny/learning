7008f65d0d034e682da3b38cfbbc2e87f530a8a5
rcpenny <rongchen0905@gmail.com>
2020-03-25 16:44:21 +0800

    oop fundamentals

---
+++ oop-foundations/foundamentals.cs
@@ -0,0 +1,70 @@
+// Objects
+All objects have...
+	• Identity: 
+	Daniel's coffee mug  
+	To separate from other objects
+	
+	• Attributes: 
+	color, size, fullness
+	To describe current state
+	
+	• Behaviors:
+	fill(), empty(), clean()
+	Things it can do
+
+// Classes
+Code-template for creating objects
+
+Class components
+	• Name(Type): 
+	What is it? RoundCookie
+	
+	• Attributes(properties, data): 
+	What describes it?
+	weight, color, icing
+	
+	• Behaviors(operations, method):
+	decorate() consume()
+
+object = instance of a particular class
+
+// A   P.I.E.
+
+// Abstraction
+Abstractions focuses on the essential qualities rather than specific  examples.
+
+e.g. politician -> George Washington
+
+Always discard unimportant qualities
+
+// Encapsulation
+Containing the elements of an object, not just keep them, but also protect them.
+
+One principle: an object should not make anything about itself available except what is absolutely necessary for other parts of the application to work.
+
+General rule: encapsulate as much as possible
+
+// Inheritance
+	• Base a new object or class on an existing one 
+	• inherit the existing attributes and methods.
+
+Good parts: 
+code reuse, one change in superclass
+
+// Polymorphism
+Dynamic Poly (run-time)
+	• Inheritance, abstract class, interface
+	• Defining different classes used with the same interface
+
+Static Poly(compile-time)
+	• Overloading
+Overload a method from the same class that can take different sets of parameters.
+
+
+// Analysis, Design and Programming
+5 steps
+	1. Gather requirements.
+	2. Describe the applications.
+	3. Identify the main objects.
+	4. Describe the interactions.
+Create a class diagram.
