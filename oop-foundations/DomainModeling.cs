a1a9c642da89a79838d34921570bcaa50371dfa2
rcpenny <rongchen0905@gmail.com>
2020-03-25 16:58:04 +0800

    domain modeling

---
+++ oop-foundations/DomainModeling.cs
@@ -0,0 +1,14 @@
+// Identifying Objects
+	• Identify nouns from use cases / stories
+Remove unnecessary nouns
+
+// Identifying class relationships
+	• Draw lines between Entities with relationships
+	• Add details on relationship lines
+	• Add additional notations on lines
+1 to many, 1 to 1, many to many etc.
+
+// CRC CARD
+	• Class (Components)
+	• Responsibilities
+Collaborators (interacting classes)
