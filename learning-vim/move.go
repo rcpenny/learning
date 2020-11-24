beea660265c30bf9e279e0b9405a46b93df0813a
rcpenny <rongchen0905@gmail.com>
2020-11-25 00:15:02 +0800

    add

---
+++ learning-vim/move.go
@@ -0,0 +1,13 @@
+// navigating with hjkl
+move around
+hjkl to move aroud
+h left j down k up l right
+
+9j will move 9 lines down.
+
+// words
+w means word, w will move forward one word
+e means end, e will move to the end of a word
+4w moves four words
+
+// find word
