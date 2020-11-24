6e2b7b4e223c220e9fe728f86c9c027bebcc4827
rcpenny <rongchen0905@gmail.com>
2020-11-24 23:49:13 +0800

    vim basics

---
+++ learning-vim/basics.go
@@ -0,0 +1,25 @@
+// start and quit vim
+
+: = Prefix for entering command-line mode
+q = shortcut for quit command
+a = All buffers
+! = Force
+
+// Modes
+insert mode for writing text
+normal mode is for manipulating text
+
+press i to enter insert mode
+press Esc to enter normal mode
+
+// insert text
+vim learn.txt
+enter insert mode and write text.
+
+// save and quit
+hit i for insert mode
+write some text.
+
+:w learn-rf.txt to write text to learn-rf.txt
+:wq will write text to learn.txt and quit
+ctrl + g  will display the file name.
